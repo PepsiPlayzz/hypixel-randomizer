9bcc0b84477963dac7b4d42ec759a35e5f7abbb3
PepsiPlayzz <pepsiplayzz69@gmail.com>
2022-06-10 00:08:56 -0600

    General Bot Updates - 6/10/22
    good stuff good stuff, even if it's incredibly minor

--- logging.js
+++ logging.js
@@ -1,6 +1,6 @@
 module.exports = {
     logDate: function(string) {
-        var date = new Date().toISOString().replace("T", " ").substring(0, 19);
+        const date = new Date().toISOString().replace("T", " ").substring(0, 19);
         console.log("[" + date + "] " + string);
     }
 }
